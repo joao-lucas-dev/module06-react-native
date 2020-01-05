448993cfbba1a335acc586547e0ee0a502dbaa0a
Joao Lucas <jlmelosouza18@gmail.com>
2020-01-05 16:16:48 -0300

    chore: react-navigation config

--- src/index.js
+++ src/index.js
@@ -1,25 +1,11 @@
 import React from 'react';
-import { SafeAreaView, Text, StyleSheet } from 'react-native';
 
 import './config/ReactotronConfig';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 20,
-  },
-});
+import Routes from './routes';
 
 const App = () => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Hello World</Text>
-    </SafeAreaView>
-  );
+  return <Routes />;
 };
 
 export default App;
