528916e689950d09565da077739d321aec5b406f
Joao Lucas <jlmelosouza18@gmail.com>
2020-01-04 22:03:42 -0300

    chore: initial state

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 20,
+  },
+});
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Hello World</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
