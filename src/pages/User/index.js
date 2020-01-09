9c47611f80bc6a4494b51801a9fe378fc238aa4e
Joao Lucas <jlmelosouza18@gmail.com>
2020-01-09 15:31:50 -0300

    feat: loading in page User

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { ActivityIndicator } from 'react-native';
 
 import PropTypes from 'prop-types';
 
@@ -29,6 +30,7 @@ export default class User extends Component {
   });
 
   state = {
+    loading: false,
     stars: [],
   };
 
@@ -36,14 +38,16 @@ export default class User extends Component {
     const { navigation } = this.props;
     const user = navigation.getParam('user');
 
+    this.setState({ loading: true });
+
     const response = await api.get(`/users/${user.login}/starred`);
 
-    this.setState({ stars: response.data });
+    this.setState({ stars: response.data, loading: false });
   }
 
   render() {
     const { navigation } = this.props;
-    const { stars } = this.state;
+    const { loading, stars } = this.state;
 
     const user = navigation.getParam('user');
 
@@ -54,6 +58,8 @@ export default class User extends Component {
           <Name>{user.name}</Name>
           <Bio>{user.bio}</Bio>
         </Header>
+
+        {loading && <ActivityIndicator color="#7159c1" />}
 
         <Stars
           data={stars}
